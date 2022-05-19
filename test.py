25b862f394873c09be8082c0949051450c993c45
Maximus Aurelius Wiranata <maximusaureliusw@gmail.com>
2022-05-19 11:44:19 +0700

    delete try

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-img = cv2.imread("bigsleep.jpg")
-text = pytesseract.image_to_string(img)
-print(text)
